541c056395ded130de6311f1548183e375ed73a7
play.js <git@gituser.com>
2020-09-01 16:11:34 -0300

    modules

---
+++ src/config/custom-express.js
@@ -0,0 +1,23 @@
+/// Encapsulamento dse modulos
+require("marko/node-require").install();
+require("marko/express");
+
+const express = require("express");
+const rotas = require("../app/rotas/rotas"); //Define o caminho para as Rotas da aplicacao.
+const bodyParser = require("body-parser");
+
+//Devolvendo o objeto para uso.
+module.exports = function () {
+
+    let app = express();
+
+    //Variável Estática
+    app.use("/estatico", express.static("src/app/public"));
+
+    //Middleware
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    rotas(app); //Chama Rotas passando app como parametro.   
+
+    return app;
+};
