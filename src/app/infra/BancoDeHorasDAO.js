6d30adda7dcb789fe66e1c03a1a8c5b67a8e2e64
Felipe Jacobucci <gotakicai@gmail.com>
2020-09-11 18:48:46 -0300

    Sqk Novo Projeto
    SQL de inserção de projeto

--- src/app/infra/BancoDeHorasDAO.js
+++ src/app/infra/BancoDeHorasDAO.js
@@ -5,5 +5,34 @@ class bancoDeHoras {
 
     /// A PARTIR DAQUI COMEÇAM OS SQLs
 
-    
+    /// INSERIR PROJETO NO BD.
+    inserirProjeto(projeto) {
+        var SQL = "";
+        SQL = "INSERT INTO produto (titulo,descricao,autor,cliente,tempoEstimado,dataPrevInicio,dataPrevFim,situacao,proposta)";
+        SQL += " VALUES(?,?,?,?,?,?,?,?,?)";
+
+        console.log(">> Criando produto novo");
+        return new Promise((resolve, reject) => {
+            this._db.run(
+                SQL, [
+                    projeto.titulo,
+                    projeto.descricao,
+                    projeto.autor,
+                    projeto.cliente,
+                    projeto.tempoEstimado,
+                    projeto.dataPrevInicio,
+                    projeto.dataPrevFim,
+                    projeto.situacao,
+                    projeto.proposta
+            ],
+                (erro) => {
+                    if (erro) return reject("ERRO AO CADASTRAR PROJETO: " + erro);
+                    resolve();
+                }
+            )
+        });
+        
+    }
+
+
 }
